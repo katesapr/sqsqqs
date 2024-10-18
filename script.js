0be852db03a1b60965faad301ade348cb2a9e364
KateSapr <katessapr@gmail.com>
2024-10-18 20:14:58 +0300

    initial

---
+++ script.js
@@ -0,0 +1,7 @@
+let login = document.querySelector("login");
+let password = document.querySelector("password");
+let sign_in = document.querySelector("sign_in");
+
+function handleClick() {
+    console.log('Вы вошли в систему');
+};
