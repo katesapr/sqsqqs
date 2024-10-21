1e7ff1e3456b7675d231b0a02b93554b55549fa9
KateSapr <katessapr@gmail.com>
2024-10-21 22:22:10 +0300

    task complited

--- script.js
+++ script.js
@@ -1,7 +1,55 @@
-let login = document.querySelector("login");
-let password = document.querySelector("password");
-let sign_in = document.querySelector("sign_in");
+document.getElementById('registrationForm').addEventListener('submit', function(event) {
+    event.preventDefault();
 
-function handleClick() {
-    console.log('Вы вошли в систему');
-};
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    let isValid = true;
+
+    
+    document.getElementById('emailError').textContent = '';
+    document.getElementById('passwordError').textContent = '';
+
+   
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailPattern.test(email)) {
+        document.getElementById('emailError').textContent = 'Некорректный email.';
+        isValid = false;
+    }
+
+
+    if (password.length < 8 || password.length > 32) {
+        document.getElementById('passwordError').textContent = 'Пароль должен содержать от 8 до 32 символов.';
+        isValid = false;
+    }
+
+
+    if (isValid) {
+        const data = {
+            email: email,
+            password: password
+        };
+
+        fetch('https://6709508caf1a3998baa11eb3.mockapi.io/api/v1/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Ошибка сети');
+            }
+            return response.json();
+        })
+        .then(data => {
+            alert('Форма отправлена!');
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('Ошибка:', error);
+            alert('Ошибка при отправке формы');
+        });
+    }
+});
